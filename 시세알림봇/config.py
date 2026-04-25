eca58024319e3143f0df4ed944f9e9e8f434c9a9
kakaopaykjc-ship-it <kakaopaykjc@gmail.com>
2026-04-25 13:14:50 +0900

    Update config.py

--- 시세알림봇/config.py
+++ 시세알림봇/config.py
@@ -1,7 +1,7 @@
 # 텔레그램 설정
 TELEGRAM_TOKEN = "여기에_봇_토큰_입력"
-CHAT_ID = "2114981316"           # 개인 채팅
-GROUP_CHAT_ID = "-5041807550"    # 코인시세알림 그룹방
+CHAT_ID = "개인 CHATID"           # 개인 채팅
+GROUP_CHAT_ID = "-그룹CHATID"    # 코인시세알림 그룹방
 
 # 날씨 지역 설정 (wttr.in 검색어)
 LOCATIONS = [
